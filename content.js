7103637159bb6ebc335ec09f3bb559af0b0d08a8
Jordan MacBook <suarez.jordan@orange.fr>
2021-02-14 18:18:37 +0100

    Enable trad if selection is not inside input element

--- content.js
+++ content.js
@@ -4,15 +4,6 @@ function getUserSelection() {
         if (userSelection.length > 2 && userSelection.length < 50) {
             return callApiForTranslate(userSelection);
         }
-
-        // document.addEventListener('click', function(event) {
-        //     const target = event.target
-        //     if(!target.matches('input')) {
-        //         if (userSelection.length > 2 && userSelection.length < 50) {
-        //             return callApiForTranslate(userSelection);
-        //         }
-        //     }
-        // })
     }
 }
 
@@ -32,27 +23,30 @@ function callApiForTranslate(valueSelected) {
 }
 
 function insertHtmlFromSelection(selectionObject, translation) {
-    if(selectionObject.getRangeAt(0).startContainer.childNodes.length === 0) {
+    const childNodes = selectionObject.getRangeAt(0).startContainer.childNodes
+    const childArray = Array.from(childNodes)
+
+    // Enable trad if selection is not inside input element
+    if (!childArray.find(element => element.matches('input'))) {
         let range;
         let expandedSelRange;
         let node;
         if (selectionObject.getRangeAt && selectionObject.rangeCount) {
-            range = selectionObject.getRangeAt(0);
-            expandedSelRange = range.cloneRange();
-            console.log(range)
-            range.collapse(false);
+        range = window.getSelection().getRangeAt(0);
+        expandedSelRange = range.cloneRange();
+        range.collapse(false);
 
-            // insert html content
-            const el = document.createElement("div");
-            el.innerHTML = `<span class="translate">[${targetLanguage.toUpperCase()}: ${translation} ]</span>`;
-            let frag = document.createDocumentFragment();
-            let node;
-            let lastNode;
-                while ((node = el.firstChild)) {
-                    lastNode = frag.appendChild(node);
-                }
-            range.insertNode(frag);
-            selectionObject.empty();
+        // insert html content
+        const el = document.createElement("div");
+        el.innerHTML = `<span class="translate">[${targetLanguage.toUpperCase()}: ${translation} ]</span>`;
+        let frag = document.createDocumentFragment();
+        let node;
+        let lastNode;
+        while ((node = el.firstChild)) {
+            lastNode = frag.appendChild(node);
+        }
+        range.insertNode(frag);
+        selectionObject.empty();
         }
     }
 }
